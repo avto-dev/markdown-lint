cf9c79fe3c1e1b4d888913371775bcbcfbc0df5a
Paramtamtam <slashkot@gmail.com>
2020-01-18 18:48:30 +0500

    WIP

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,23 +2,23 @@
 
 RUN_ARGS="";
 
-if [ ! -z $INPUT_RULES ]; then
+if [ ! -z $INPUT_RULES ] && [ "$INPUT_RULES" != "" ]; then
   RUN_ARGS="$RUN_ARGS --rules $INPUT_RULES";
 fi;
 
-if [ ! -z $INPUT_CONFIG ]; then
+if [ ! -z $INPUT_CONFIG ] && [ "$INPUT_CONFIG" != "" ]; then
   RUN_ARGS="$RUN_ARGS --config $INPUT_CONFIG";
 fi;
 
-if [ ! -z $INPUT_FIX ]; then
+if [ ! -z $INPUT_FIX ] && [ "$INPUT_FIX" = "true" ]; then
   RUN_ARGS="$RUN_ARGS --fix";
 fi;
 
-if [ ! -z $INPUT_OUTPUT ]; then
+if [ ! -z $INPUT_OUTPUT ] && [ "$INPUT_OUTPUT" != "" ]; then
   RUN_ARGS="$RUN_ARGS --output $INPUT_OUTPUT";
 fi;
 
-if [ ! -z $INPUT_IGNORE ]; then
+if [ ! -z $INPUT_IGNORE ] && [ "$INPUT_IGNORE" != "" ]; then
   RUN_ARGS="$RUN_ARGS --ignore $INPUT_IGNORE";
 fi;
 
