4836b5e22e6cb5fad9abc93cddd38d762f4336f7
Alexander Grund <Flamefire@users.noreply.github.com>
2020-07-25 14:04:39 +0200

    Fix variable checks and argument splitting

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,24 +3,26 @@ shopt -s globstar
 
 RUN_ARGS="";
 
-if [ ! -z $INPUT_RULES ] && [ "$INPUT_RULES" != "" ]; then
+if [ "$INPUT_RULES" != "" ]; then
   RUN_ARGS="$RUN_ARGS --rules $INPUT_RULES";
 fi;
 
-if [ ! -z $INPUT_CONFIG ] && [ "$INPUT_CONFIG" != "" ]; then
+if [ "$INPUT_CONFIG" != "" ]; then
   RUN_ARGS="$RUN_ARGS --config $INPUT_CONFIG";
 fi;
 
-if [ ! -z $INPUT_FIX ] && [ "$INPUT_FIX" = "true" ]; then
+if [ "$INPUT_FIX" = "true" ]; then
   RUN_ARGS="$RUN_ARGS --fix";
 fi;
 
-if [ ! -z $INPUT_OUTPUT ] && [ "$INPUT_OUTPUT" != "" ]; then
+if [ "$INPUT_OUTPUT" != "" ]; then
   RUN_ARGS="$RUN_ARGS --output $INPUT_OUTPUT";
 fi;
 
-if [ ! -z $INPUT_IGNORE ] && [ "$INPUT_IGNORE" != "" ]; then
+if [ "$INPUT_IGNORE" != "" ]; then
   RUN_ARGS="$RUN_ARGS --ignore $INPUT_IGNORE";
 fi;
 
+# Do not quote "$@" as Github Actions passes each argument as a single arg.
+# So 'args: --fix foo bar.md'  would be treated as a single string and not be parsed by markdownlint
 exec /usr/local/bin/markdownlint $RUN_ARGS $@
